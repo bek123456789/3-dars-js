bdf5d6fab1d681a2d8b690a294d177be6d946730
bek123456789 <redmirating@gmail.com>
2024-03-14 11:21:17 +0500

    done

---
+++ node.js
@@ -0,0 +1,9 @@
+alert("Sizda hozir random sonlar chiqishni boshlaydi")
+let random_num = (Math.round)(Math.random() * 100);
+
+alert(`random orqali chiqgan son ${random_num}`);
+
+alert("biz random  sonni 2 ga kopaytiramiz va bo'lamiz")
+
+alert(`Random orqali chiqgan son ${random_num} \n biz uni 2 ga ko'paytirib ${random_num * 2} chiqardik \n biz uni 2 ga bo'lib ${random_num / 2} chiqardik \n biz uni 3ga bolgandigi qoldiq ${random_num % 3} `)
+
